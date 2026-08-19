9fad23dcf593bc9684e8e810d5073b9db27ad02a
Janavi Gandikota <janavigandikota@gmail.com>
2026-08-19 21:47:50 +0530

    Day 12: Learn and practice nested loops

---
+++ Day12-Java Basics/Matrix.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Matrix {
+    public static void main(String[]args){
+        Scanner scanner = new Scanner(System.in);
+        int rows;int columns;
+        char symbol;
+        System.out.println("Enter the number of rows");
+        rows = scanner.nextInt();
+        
+        System.out.println("Enter the number of columns:");
+        columns = scanner.nextInt();
+        
+        System.out.println("Enter the symbol to use:");
+        symbol = scanner.next().charAt(0);
+
+        for(int i = 0;i< rows;i++){
+        for(int j = 0;j < columns;j++){
+            System.out.print(symbol);
+        }
+        System.out.println();
+    }
+        scanner.close();
+    }
+}
---
+++ Day12-Java Basics/Nestedloop.java
@@ -0,0 +1,12 @@
+public class Nestedloop {
+    public static void main(String[]args){
+       //nested loop = a loop inside another loop
+       //used often with matrices or DSA
+       for(int i = 1;i<=3;i++){
+            for(int j = 0;j<=9;j++){ 
+                System.out.println(j+"");
+            }
+            System.out.println();
+       } 
+    }    
+}
